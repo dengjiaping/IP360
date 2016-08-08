7fb16bbc3d606bb6c94df29dd38c77e596cb1203
duanjunjie <15210391776@163.com>
2016-08-08 08:46:42 +0800

    no message

--- src/com/truthso/ip360/viewholder/ViewHolder.java
+++ src/com/truthso/ip360/viewholder/ViewHolder.java
@@ -1,8 +1,5 @@
 package com.truthso.ip360.viewholder;
 
-import com.truthso.ip360.utils.ImageLoader;
-import com.truthso.ip360.utils.ImageLoader.Type;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.SparseArray;
@@ -11,6 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.truthso.ip360.utils.ImageLoader;
+import com.truthso.ip360.utils.ImageLoader.Type;
+
 
 public class ViewHolder {
 	private final SparseArray<View> mViews;
