d1d01a6c4c9e8ea4d8918504f7c7f31717e578e2
宝宝爱吃瓜 <960894632@qq.com>
2016-10-30 21:47:08 +0800

    xiazai

--- src/com/truthso/ip360/updownload/DownLoadHelper.java
+++ src/com/truthso/ip360/updownload/DownLoadHelper.java
@@ -2,7 +2,9 @@ package com.truthso.ip360.updownload;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
+import android.support.v4.util.ArrayMap;
 import android.util.Log;
 
 import com.alibaba.sdk.android.oss.ClientException;
@@ -17,26 +19,31 @@ import com.alibaba.sdk.android.oss.model.GetObjectRequest;
 import com.alibaba.sdk.android.oss.model.GetObjectResult;
 import com.alibaba.sdk.android.oss.model.Range;
 import com.truthso.ip360.application.MyApplication;
+import com.truthso.ip360.dao.UpDownLoadDao;
 
 public class DownLoadHelper {
 
 	private String endpoint = "http://oss-cn-beijing.aliyuncs.com";
 	private OSS oss;
-	private OSSAsyncTask task;
-
-	public DownLoadHelper() {
+    private DownLoadHelper helper=new DownLoadHelper();
+    private Map<String, OSSAsyncTask> taskMap=new ArrayMap<String, OSSAsyncTask>();
+	private  DownLoadHelper() {
 		OSSCredentialProvider credentialProvider = new OSSPlainTextAKSKCredentialProvider("LTAIIHLk9eURcRim", "6rXdqbwAShL0P8uR4L1zoLVX4eIUKj");
 		oss = new OSSClient(MyApplication.getApplication(), endpoint, credentialProvider);
 	}
 
+	public  DownLoadHelper  getInstance(){
+		return helper;
+	}
+		
 	// 明文设置secret的方式建议只在测试时使用，更多鉴权模式请参考后面的`访问控制`章节
 
-	public void downloadFile(String objectKey,long position) {
+	public void downloadFile(final String objectKey,long position) {
 		GetObjectRequest get = new GetObjectRequest("ip360-test",objectKey);
 		// 设置范围
 		get.setRange(new Range(position, Range.INFINITE)); // 下载0到99字节共100个字节，文件范围从0开始计算
 
-		task = oss.asyncGetObject(get, new OSSCompletedCallback<GetObjectRequest, GetObjectResult>() {	
+		OSSAsyncTask task = oss.asyncGetObject(get, new OSSCompletedCallback<GetObjectRequest, GetObjectResult>() {	
 			
 			@Override
 			public void onSuccess(GetObjectRequest request, GetObjectResult result) {
@@ -44,12 +51,20 @@ public class DownLoadHelper {
 				InputStream inputStream = result.getObjectContent();
 				byte[] buffer = new byte[2048];
 				int len;
+				int total = 0;
 				try {
 					while ((len = inputStream.read(buffer)) != -1) {
 						// 处理下载的数据
+						total+=len;
+						
 					}
+					
 				} catch (IOException e) {
 					e.printStackTrace();
+					
+				}finally{
+					Log.i("djj", "total"+total);
+					UpDownLoadDao.getDao().updateDownLoadProgress(objectKey, total);
 				}
 			}
 
@@ -71,11 +86,13 @@ public class DownLoadHelper {
 	
 		});
 		
-		
+		taskMap.put(objectKey, task);
 	}
 	
-	public void pauseDownload(){
+	public void pauseDownload(String objectKey){
 		
+		OSSAsyncTask ossAsyncTask = taskMap.get(objectKey);
+		taskMap.remove(objectKey).cancel();
 	}
 	
 
